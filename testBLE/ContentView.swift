a1b038ba3b5536679e57f7a9da7d1ca20c5a7309
Toshiki Nagahama <exevolute@gmail.com>
2021-08-06 09:41:04 +0900

    Initial Commit

---
+++ testBLE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testBLE
+//
+//  Created by Toshiki Nagahama on 2021/08/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testBLE/testBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  testBLEApp.swift
+//  testBLE
+//
+//  Created by Toshiki Nagahama on 2021/08/06.
+//
+
+import SwiftUI
+
+@main
+struct testBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
